80e022ab17b286b3c41e08e7317dae22b9f41817
Martin Duvanel <martin.duvanel@gmail.com>
2016-06-25 14:58:48 +0200

    Style + typos in ControllerRunnable

--- app/src/main/java/stargame/android/controller/ControllerRunnable.java
+++ app/src/main/java/stargame/android/controller/ControllerRunnable.java
@@ -11,110 +11,111 @@ import stargame.android.util.Orientation;
 
 /**
  * Class ControllerThread handles threaded execution of DPadController actions.
- * 
+ *
  * @author Duduche
  */
 public class ControllerRunnable implements Runnable
 {
-	private DPadController mControler;
+    private DPadController mController;
 
-	private boolean mRun;
+    private boolean mRun;
 
-	static private class Action
-	{
-		public DPadAction mAction;
+    static private class Action
+    {
+        public DPadAction mAction;
 
-		public Orientation mOrientation;
+        public Orientation mOrientation;
 
-		public Action( DPadAction eAction, Orientation eOrientation )
-		{
-			mOrientation = eOrientation;
-			mAction = eAction;
-		}
-	}
+        public Action( DPadAction eAction, Orientation eOrientation )
+        {
+            mOrientation = eOrientation;
+            mAction = eAction;
+        }
+    }
 
-	private ArrayList< Action > mActionsQueue;
+    private ArrayList< Action > mActionsQueue;
 
-	public ControllerRunnable( DPadController oControler )
-	{
-		mRun = true;
-		mControler = oControler;
-		mActionsQueue = new ArrayList< Action >();
-	}
+    public ControllerRunnable( DPadController oController )
+    {
+        mRun = true;
+        mController = oController;
+        mActionsQueue = new ArrayList< Action >();
+    }
 
-	public void AddCommand( DPadAction eAction, Orientation eOrientation )
-	{
-		synchronized ( mActionsQueue )
-		{
-			mActionsQueue.add( new Action( eAction, eOrientation ) );
-		}
-	}
+    public void AddCommand( DPadAction eAction, Orientation eOrientation )
+    {
+        synchronized ( mActionsQueue )
+        {
+            mActionsQueue.add( new Action( eAction, eOrientation ) );
+        }
+    }
 
-	public void run()
-	{
-		Action oAction = null;
-		boolean bActionAvailable;
+    public void run()
+    {
+        Action oAction = null;
+        boolean bActionAvailable;
 
-		try
-		{
-			while ( mRun )
-			{
-				bActionAvailable = false;
+        try
+        {
+            while ( mRun )
+            {
+                bActionAvailable = false;
 
-				synchronized ( mActionsQueue )
-				{
-					if ( !mActionsQueue.isEmpty() )
-					{
-						bActionAvailable = true;
-						oAction = mActionsQueue.get( 0 );
-						mActionsQueue.remove( 0 );
-					}
-				}
+                synchronized ( mActionsQueue )
+                {
+                    if ( !mActionsQueue.isEmpty() )
+                    {
+                        bActionAvailable = true;
+                        oAction = mActionsQueue.get( 0 );
+                        mActionsQueue.remove( 0 );
+                    }
+                }
 
-				if ( bActionAvailable )
-				{
-					switch ( oAction.mAction )
-					{
-					case DPAD_UP:
-						mControler.UpAction( oAction.mOrientation );
-						break;
-					case DPAD_DOWN:
-						mControler.DownAction( oAction.mOrientation );
-						break;
-					case DPAD_LEFT:
-						mControler.LeftAction( oAction.mOrientation );
-						break;
-					case DPAD_RIGHT:
-						mControler.RightAction( oAction.mOrientation );
-						break;
-					case DPAD_CENTER:
-						mControler.CenterAction();
-						break;
-					case DPAD_BACK:
-						mControler.BackAction();
-						break;
-					}
-				}
-				else
-				{
-					Thread.sleep( 40 );
-				}
-			}
-		}
-		catch ( Exception e )
-		{
-			Logger.e( String.format( "Exception caught in Controler Thread: %s", Log.getStackTraceString( e ) ) );
-		}
-	}
+                if ( bActionAvailable )
+                {
+                    switch ( oAction.mAction )
+                    {
+                        case DPAD_UP:
+                            mController.UpAction( oAction.mOrientation );
+                            break;
+                        case DPAD_DOWN:
+                            mController.DownAction( oAction.mOrientation );
+                            break;
+                        case DPAD_LEFT:
+                            mController.LeftAction( oAction.mOrientation );
+                            break;
+                        case DPAD_RIGHT:
+                            mController.RightAction( oAction.mOrientation );
+                            break;
+                        case DPAD_CENTER:
+                            mController.CenterAction();
+                            break;
+                        case DPAD_BACK:
+                            mController.BackAction();
+                            break;
+                    }
+                }
+                else
+                {
+                    Thread.sleep( 40 );
+                }
+            }
+        }
+        catch ( Exception e )
+        {
+            Logger.e( String.format( "Exception caught in Controller Thread: %s",
+                                     Log.getStackTraceString( e ) ) );
+        }
+    }
 
-	public void doStop()
-	{
-		mRun = false;
-		mControler = null;
-	}
+    public void doStop()
+    {
+        mRun = false;
+        mController = null;
+    }
 
-	public void SaveState( Bundle oMap )
-	{
-		mControler.SaveState( oMap );
-	}
+    public void SaveState( Bundle oMap )
+    {
+        mController.SaveState( oMap );
+    }
 }

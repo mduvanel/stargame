d44095ecc021260a7e9f089b28b858e0b41cbaa3
Martin Duvanel <martin.duvanel@gmail.com>
2017-03-25 14:18:23 +0100

    Project versions updated

--- app/src/test/java/dummyTestTest.java
+++ app/src/test/java/dummyTestTest.java
@@ -2,15 +2,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by Duduche on 17.11.16.
- */
 public class dummyTestTest
 {
     @Test
     public void dummyTest() throws Exception
     {
-
+        assertTrue( true );
     }
-
 }
